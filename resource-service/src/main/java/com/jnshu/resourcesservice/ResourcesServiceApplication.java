cd92e63c6b724de4115891f0afd4981b6d950caa
chedou <32168442+chedou@users.noreply.github.com>
2018-12-13 00:10:48 +0800

    新建后台资源持有端

---
+++ resource-service/src/main/java/com/jnshu/resourcesservice/ResourcesServiceApplication.java
@@ -0,0 +1,14 @@
+package com.jnshu.resourcesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourcesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourcesServiceApplication.class, args);
+	}
+
+}
+
---
+++ resource-service/src/test/java/com/jnshu/resourcesservice/ResourcesServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.jnshu.resourcesservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ResourcesServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
