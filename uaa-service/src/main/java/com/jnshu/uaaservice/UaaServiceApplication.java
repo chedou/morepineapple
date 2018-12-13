6121ad621c3fc28be96da4f462db6d1674a4532a
chedou <32168442+chedou@users.noreply.github.com>
2018-12-13 17:54:56 +0800

    update code

--- uaa-service/src/main/java/com/jnshu/uaaservice/UaaServiceApplication.java
+++ uaa-service/src/main/java/com/jnshu/uaaservice/UaaServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.eureka.*;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableEurekaClient
-// @EnableAutoConfiguration
 public class UaaServiceApplication {
 
 	public static void main(String[] args) {
