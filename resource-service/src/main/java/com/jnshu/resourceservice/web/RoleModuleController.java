24eb34d4b8a33a6808bcf7a4ab291cb90cfcc1f2
chedou <32168442+chedou@users.noreply.github.com>
2018-12-23 21:31:31 +0800

    update code

--- resource-service/src/main/java/com/jnshu/resourceservice/web/RoleModuleController.java
+++ resource-service/src/main/java/com/jnshu/resourceservice/web/RoleModuleController.java
@@ -7,4 +7,17 @@ package com.jnshu.resourceservice.web;
  * @create: 2018-12-22 20:57
  **/
 public class RoleModuleController {
+
+	//TODO:橘色管理-新增角色
+
+	//TODO:橘色管理-更改角色信息
+
+	//TODO:橘色管理-删除角色
+
+	//TODO:橘色管理-查询单个角色
+
+	//TODO:橘色管理-查询角色列表
+
+
+
 }
